682694b8186ea4b95eec34edd71952b2706043d6
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-18 15:45:56 +0100

    Ex 08 Tableaux listes enum

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabEx08
+{
+    class Program
+    {
+        enum week { Lundi=1, Mardi, Mercredi, Jeudi, Vendredi, Samedi, Dimanche }
+        
+
+        static void Main(string[] args)
+        {
+            foreach (string allTheWeek in Enum.GetNames(typeof(week)))
+            {
+                Console.WriteLine(allTheWeek);
+            }
+
+            /*week lundi = week.Lundi;
+            Enum.Parse(string lundi) = "1";*/
+            
+
+            Console.WriteLine(Environment.NewLine + Enum.GetName(typeof(week), 4));
+
+        }
+    }
+}
